5339e3d72933c068a4cc99bc417bde1545295efc
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-10-03 17:03:16 -0500

    Updates to kernel_main

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,9 +1,8 @@
 #include "rprintf.h"
 // #include "esp_printf.h"
-#include <stdio.h>
 #include <stdint.h>
 
-#define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
+#define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
 
 const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
 
@@ -13,18 +12,21 @@ uint8_t inb (uint16_t _port) {
     return rv;
 }
 
+
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+#define VGA_ADDRESS 0xb8000
+
 struct termbuf {
     char ASCII;
     char COLOR;
 };
 
-#define VGA_WIDTH 80
-#define VGA_HEIGHT 25
-#define DEFAULT_COLOR 7 // grey on black 
-#define VIDEO_MEM ((struct termbuf*)0xb8000)
-
 int x = 0;
 int y = 0;
+
+
 
 void print_string(void (*pc)(char), char *s) {
     while (*s != 0) {
@@ -33,59 +35,56 @@ void print_string(void (*pc)(char), char *s) {
     }
 }
 
-void print_char(char c) {
-    struct termbuf *vram = (struct termbuf *)0xB8000;
-    vram[x].ASCII = c;
-    vram[x].COLOR = 7;
-    x++;
-}
+// void print_char(char c) {
+//     struct termbuf *vram = (struct termbuf *)0xB8000;
+//     vram[x].ASCII = c;
+//     vram[x].COLOR = 7;
+//     x++;
+// }
+
+// Pointer to the start of video memory
+struct termbuf* const vram = (struct termbuf*)VGA_ADDRESS;
 
 void scroll() {
     // Scroll the screen up by one row
-    for (int y = 1; y < VGA_HEIGHT; y++) {
-        for (int x = 0; x < VGA_WIDTH; x++) {
-            VIDEO_MEM[(y - 1) * VGA_WIDTH + x] = VIDEO_MEM[y * VGA_WIDTH + x];
+    for (int x = 1; x < VGA_HEIGHT; x++) {
+        for (int y = 0; y < VGA_WIDTH; y++) {
+            vram[(x - 1) * VGA_WIDTH + y] = vram[x * VGA_WIDTH + y];
         }
     }
     // Clear the last row
-    for ( int x = 0; x < VGA_WIDTH; x++) {
-        VIDEO_MEM[(VGA_HEIGHT - 1) * VGA_WIDTH + x].ASCII = ' ';
-        VIDEO_MEM[(VGA_HEIGHT - 1) * VGA_WIDTH + x].COLOR = DEFAULT_COLOR;
-    }
-    if (x >= VGA_HEIGHT) {
-        x = VGA_HEIGHT - 1;
+    for (int y = 0; y < VGA_WIDTH; y++) {
+        vram[(VGA_HEIGHT - 1) * VGA_WIDTH + y].ASCII = ' ';
+        vram[(VGA_HEIGHT - 1) * VGA_WIDTH + x].COLOR = 7;
     }
 }
 
 
-void putc(char c) {
-    struct termbuf *vram = (struct termbuf *)0xB8000;
-
-    if (c == '\n') {
-        x = (x / VGA_WIDTH + 1) * VGA_WIDTH; // Move to the start of the next line
-    } else if (c == '\r') {
-        x = (x / VGA_WIDTH) * VGA_WIDTH; // Move to the start of the current line
+int putc(int ch) {
+    if (ch == '\n') {
+        x++;
+        y = 0;
     } else {
-        vram[x].ASCII = c;
-        vram[x].COLOR = DEFAULT_COLOR;
+        vram[x * VGA_WIDTH + y].ASCII = ch;
+        vram[x * VGA_WIDTH + y].COLOR = 7; 
         x++;
     }
 
-    if (x >= VGA_WIDTH * VGA_HEIGHT) {
+
+    if (y >= VGA_WIDTH) {
         scroll();
-        x = (VGA_HEIGHT - 1) * VGA_WIDTH; // Move to the start of the last line
+        x = VGA_HEIGHT - 1;
     }
+
+    return ch;
 }
 
 
 
 void main() {
 
-    print_string(putc, "Hello, kernel World!\n");
+    esp_printf(putc, "Hello, kernel World!\n");
 
-
-    unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
-    const unsigned char color = 7; // gray text on black background
 
     while(1) {
         uint8_t status = inb(0x64);
@@ -97,7 +96,7 @@ void main() {
                 continue; // Ignore key releases
             }
             
-            esp_printf(print_char, "0x%02x\n    %c\n", scancode);
+            esp_printf(putc, "0x%02x\n    %c\n", scancode);
             // keyboard_map[scancode]
         }
     }
