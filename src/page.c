8b280c3e8b35e2831144e12d13bf5a6eaca9af68
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-10-20 17:17:46 -0500

    Skeleton added to page.c and page.h

---
+++ src/page.c
@@ -0,0 +1,25 @@
+struct ppage physical_page_array[128]; // 128 pages, each 2mb in length covers 256 megs of memory
+
+void init_pfa_list(void) {
+    // will initialze the linked list of free pages
+    // It will loop through every element of your 
+    // statically allocated physical_page_array and
+    // link it into the list
+}
+
+struct ppage *allocate_physical_pages(unsigned int npages) {
+    // allocates one or more physical pages
+    // from the list of free pages. The input 
+    // to this function npages specifies the 
+    // number of pages to allocate. The function
+    // will unlink the requested number of pages
+    // from the free list and create a new separate
+    // list. It will return a pointer to the new list
+    // (called allocd_list)
+}
+
+void free_physical_pages(struct ppage *ppage_list) {
+    // returns a list of physical pages to the 
+    // free list. Basically does the opposite of 
+    // allocate_physical_pages
+}
---
+++ src/page.h
@@ -0,0 +1,5 @@
+struct {
+    struct ppage *next;
+    struct ppage *prev;
+    void *physical_addr;
+};
