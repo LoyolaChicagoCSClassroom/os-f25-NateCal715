ab622f756ea7544b79a20f4c7e42f069d58b9d13
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-10-20 18:58:39 -0500

    Updated page.c

--- src/page.c
+++ src/page.c
@@ -6,8 +6,10 @@
 // All the functions that are written for the page allocator are going to call
 // the linked list functions to actually link and unlink elements from the list
 
+// Statically Allocate an array of struct ppages
 struct ppage physical_page_array[128]; // 128 pages, each 2mb in length covers 256 megs of memory
 
+// Initializes the list of free physical page structures
 void init_pfa_list(void) {
     // will initialze the linked list of free pages
     // It will loop through every element of your 
@@ -16,6 +18,7 @@ void init_pfa_list(void) {
     
 }
 
+// Allocates one or more physical pages from the free list
 struct ppage *allocate_physical_pages(unsigned int npages) {
     // allocates one or more physical pages
     // from the list of free pages. The input 
@@ -36,6 +39,7 @@ struct ppage *allocate_physical_pages(unsigned int npages) {
     return allocd_list;
 }
 
+// Frees one or more physical pages back to the free list
 void free_physical_pages(struct ppage *ppage_list) {
     // returns a list of physical pages to the 
     // free list. Basically does the opposite of 
@@ -43,5 +47,5 @@ void free_physical_pages(struct ppage *ppage_list) {
     if (ppage_list == NULL) {
         return; // Nothing to free
     }
-    
+
 }
