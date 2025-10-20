4331c94fc1c3ca8836e3e1ece9da5f3aae6f8470
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-10-20 18:54:11 -0500

    Updates to page.c

--- src/page.c
+++ src/page.c
@@ -1,5 +1,10 @@
+// PAGE FRAME ALLOCATOR
+
 #include "page.h"
 #include <stdio.h>
+
+// All the functions that are written for the page allocator are going to call
+// the linked list functions to actually link and unlink elements from the list
 
 struct ppage physical_page_array[128]; // 128 pages, each 2mb in length covers 256 megs of memory
 
@@ -20,10 +25,23 @@ struct ppage *allocate_physical_pages(unsigned int npages) {
     // from the free list and create a new separate
     // list. It will return a pointer to the new list
     // (called allocd_list)
+    if (npages == 0) {
+        return NULL; // No pages requested
+    }
+
+    struct ppage *allocd_list = NULL;
+
+
+
+    return allocd_list;
 }
 
 void free_physical_pages(struct ppage *ppage_list) {
     // returns a list of physical pages to the 
     // free list. Basically does the opposite of 
     // allocate_physical_pages
+    if (ppage_list == NULL) {
+        return; // Nothing to free
+    }
+    
 }
