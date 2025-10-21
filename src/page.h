ca0671cf9a8b43e87fad7da31d13b26b764bd9c3
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-10-20 19:35:09 -0500

    Added list_add and list_remove to page.h

--- src/page.h
+++ src/page.h
@@ -1,5 +1,30 @@
-struct ppage{
+#include <stdio.h>
+
+struct ppage {
     struct ppage *next;
     struct ppage *prev;
     void *physical_addr;
 };
+
+list_add(struct ppage *new_page, struct ppage **head) {
+    if (*head == NULL) {
+        *head = new_page;
+    } else {
+        new_page->next = *head;
+        (*head)->prev = new_page;
+        *head = new_page;
+    }
+}
+
+list_remove(struct ppage *page, struct ppage **head) {
+    if (page->prev) {
+        page->prev->next = page->next;
+    } else {
+        *head = page->next;
+    }
+    if (page->next) {
+        page->next->prev = page->prev;
+    }
+    page->next = NULL;
+    page->prev = NULL;
+}
