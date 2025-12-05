643e40fbaa84dd076119a30dd2f29c49e363d5f1
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-12-05 09:37:35 -0600

    Updated kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,7 +1,4 @@
 #include <stdint.h>
-// #include <stdio.h>
-// #include <fcntl.h>
-// #include <unistd.h>
 #include "rprintf.h"
 #include "fat.h"
 #include "ide.h"
@@ -12,7 +9,6 @@
 #define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
 #define SECTOR_SIZE 512
 
-// Multiboot2 header for GRUB
 const unsigned int multiboot_header[] __attribute__((section(".multiboot"))) = {
     MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16 + MULTIBOOT2_HEADER_MAGIC), 0, 12
 };
@@ -23,117 +19,61 @@ uint8_t inb(uint16_t _port) {
     return rv;
 }
 
-
-
-// #define VGA_WIDTH 80
-// #define VGA_HEIGHT 25
-// #define VGA_ADDRESS 0xb8000
-
-// struct termbuf {
-//     char ASCII;
-//     char COLOR;
-// };
-
-// static int row_x = 0;
-// static int col_y = 0;
-
-
-// // Pointer to the start of video memory
-// static struct termbuf* const vram = (struct termbuf*)VGA_ADDRESS;
-
-// void scroll() {
-//     // Scroll the screen up by one row
-//     for (int x = 1; x < VGA_HEIGHT; x++) {
-//         for (int y = 0; y < VGA_WIDTH; y++) {
-//             vram[(x - 1) * VGA_WIDTH + y] = vram[x * VGA_WIDTH + y];
-//         }
-//     }
-//     // Clear the last row
-//     for (int y = 0; y < VGA_WIDTH; y++) {
-//         vram[(VGA_HEIGHT - 1) * VGA_WIDTH + y].ASCII = ' ';
-//         vram[(VGA_HEIGHT - 1) * VGA_WIDTH + y].COLOR = 7;
-//     }
-// }
-
-
 int MyPutC(int ch) {   
-    // // Handle newline character
-    // if (ch == '\n') {
-    //     row_x++;
-    //     col_y = 0;
-    // } else {
-    //     vram[row_x * VGA_WIDTH + col_y].ASCII = (char)ch;
-    //     vram[row_x * VGA_WIDTH + col_y].COLOR = 7; 
-    //     col_y++;
-    // }
-
-    // // Move to the next column
-    // if (col_y >= VGA_WIDTH) {
-    //     col_y = 0;
-    //     row_x++;
-    // }
-    // // Scroll if we reach the bottom of the screen
-    // if (row_x >= VGA_HEIGHT) {
-    //     scroll();
-    //     row_x = VGA_HEIGHT - 1;
-    // }
-
-    // return ch;
     return ansi_putc(ch);
 }
 
-    // int print_string(void (*pc)(char), char *s) {
-    //     // Print each character until null terminator
-    //     while (*s != 0) {
-    //         uint8_t status = inb(0x64);
-    //         pc(*s);
-    //         s++;
-    //     }
-    //     return 0;
-    // }
-
-
 int root_dir_region_start = 0;
+int data_region_start = 0;
 char boot_sector[512];
 char root_directory_region[512];
+char fat_table[512 * 32];  // Cache for FAT table
+
+extern void *memcpy(void *dest, const void *src, size_t n);
 
 void sector_read(unsigned int lba, void *buffer) {
-    // Read a sector from the disk image into the buffer
     if (ata_lba_read(lba, (unsigned char *)buffer, 1) != 0) {
         esp_printf(MyPutC, "Error reading sector %d\n", lba);
     }
 }
 
-void fatInit() {
-    // Read boot sector and RDE region
+void sector_write(unsigned int lba, void *buffer) {
+    // Note: You'll need to implement ata_lba_write in ide.c
+    // For now, this is a placeholder
+    (void)lba;
+    (void)buffer;
+}
 
+void fatInit() {
     sector_read(2048, boot_sector);
 
-    esp_printf(MyPutC, "Number of sectors per cluster = %d\n", ((struct boot_sector*)boot_sector)->num_sectors_per_cluster);
-    esp_printf(MyPutC, "Number of bytes per sector = %d\n", ((struct boot_sector*)boot_sector)->bytes_per_sector);
-    esp_printf(MyPutC, "Number of reserved sectors = %d\n", ((struct boot_sector*)boot_sector)->num_reserved_sectors);
-    esp_printf(MyPutC, "Number of FAT tables = %d\n", ((struct boot_sector*)boot_sector)->num_fat_tables);
-    esp_printf(MyPutC, "Number of RDEs = %d\n", ((struct boot_sector*)boot_sector)->num_root_dir_entries);
+    struct boot_sector *bs = (struct boot_sector*)boot_sector;
 
     root_dir_region_start = 2048
-                        + ((struct boot_sector*)boot_sector)->num_reserved_sectors
-                        + ((struct boot_sector*)boot_sector)->num_fat_tables * ((struct boot_sector*)boot_sector)->num_sectors_per_fat;
+                        + bs->num_reserved_sectors
+                        + bs->num_fat_tables * bs->num_sectors_per_fat;
 
-    esp_printf(MyPutC, "Root directory region start (sectors) = %d\n", root_dir_region_start);
+    int root_dir_sectors = (bs->num_root_dir_entries * 32 + bs->bytes_per_sector - 1) / bs->bytes_per_sector;
+    data_region_start = 2048 + bs->num_reserved_sectors 
+                        + (bs->num_fat_tables * bs->num_sectors_per_fat) 
+                        + root_dir_sectors;
 
     sector_read(root_dir_region_start, root_directory_region);
     
+    // Load FAT table into cache
+    int fat_start = 2048 + bs->num_reserved_sectors;
+    for (int i = 0; i < bs->num_sectors_per_fat && i < 32; i++) {
+        sector_read(fat_start + i, fat_table + (i * 512));
+    }
 }
 
-// fatOpen()
-// Find the RDE for a file given a path
 rde * fatOpen(char *path) {
-
     rde *rde_entries = (rde *)root_directory_region;
 
-    char target_name[8]; for (int i = 0 ; i < 8; i++) target_name[i] = ' ';
-    char target_ext[3]; for (int i = 0 ; i < 3; i++) target_ext[i] = ' ';
-
+    char target_name[8]; 
+    for (int i = 0; i < 8; i++) target_name[i] = ' ';
+    char target_ext[3]; 
+    for (int i = 0; i < 3; i++) target_ext[i] = ' ';
 
     int dot_pos = -1;
     for (int i = 0; path[i] != '\0'; i++) {
@@ -143,7 +83,7 @@ rde * fatOpen(char *path) {
         }
     }
 
-    int name_len = (dot_pos >= 0) ?  dot_pos : 0;
+    int name_len = (dot_pos >= 0) ? dot_pos : 0;
     if (dot_pos < 0) {
         while (path[name_len] != '\0') name_len++;
     }
@@ -165,16 +105,9 @@ rde * fatOpen(char *path) {
 
     int max_entries = ((struct boot_sector*)boot_sector)->num_root_dir_entries;
 
-    //Iterate through the RDE region searching for a file's RDE
     for (int k = 0; k < max_entries; k++) {
-
         if (rde_entries[k].file_name[0] == 0x00) break;
-
         if ((unsigned char)rde_entries[k].file_name[0] == 0xE5) continue;
-
-        // esp_printf(MyPutC, "File name: \"%s.%s\"\n", rde_entries[k].file_name, rde_entries[k].file_extension);
-        // esp_printf(MyPutC, "Data cluster: %d\n", rde_entries[k].cluster);
-        // esp_printf(MyPutC, "File size: %d\n", rde_entries[k].file_size);
 
         int name_match = 1;
         for (int i = 0; i < 8; i++) {
@@ -193,39 +126,27 @@ rde * fatOpen(char *path) {
         }
 
         if (name_match && ext_match) {
-            // esp_printf(MyPutC, "Found matching file!\n");
             return &rde_entries[k];
         }
     }
-    return (rde*)0; // Return NULL if file not found
+    return (rde*)0;
 }
 
 int fatRead(rde *rde_ptr, char * buf, int n) {
-    // read file data into buf from file described by rde
     if (rde_ptr == (rde*)0) {
-        return -1; // Error: invalid RDE pointer
+        return -1;
     }
     
-    // Calculate data region start
-    // Data region starts after: boot sector + FAT tables + root directory
     struct boot_sector *bs = (struct boot_sector*)boot_sector;
-    int root_dir_sectors = (bs->num_root_dir_entries * 32 + bs->bytes_per_sector - 1) / bs->bytes_per_sector;
-    int data_region_start = 2048 + bs->num_reserved_sectors 
-                        + (bs->num_fat_tables * bs->num_sectors_per_fat) 
-                        + root_dir_sectors;
     
-    // Calculate the sector for this cluster
-    // Cluster 2 is the first data cluster
-    uint16_t cluster = rde_ptr -> cluster;
-    int first_sector = data_region_start + (cluster - 2) * bs -> num_sectors_per_cluster;
+    uint16_t cluster = rde_ptr->cluster;
+    int first_sector = data_region_start + (cluster - 2) * bs->num_sectors_per_cluster;
     
-    // Read the data (simplified: only reading first cluster)
     char cluster_buf[CLUSTER_SIZE];
-    for (int i = 0; i < bs -> num_sectors_per_cluster; i++) {
+    for (int i = 0; i < bs->num_sectors_per_cluster; i++) {
         sector_read(first_sector + i, cluster_buf + (i * SECTOR_SIZE));
     }
     
-    // Copy to output buffer
     int bytes_to_copy = (n < (int)rde_ptr->file_size) ? n : (int)rde_ptr->file_size;
     if (bytes_to_copy > CLUSTER_SIZE) bytes_to_copy = CLUSTER_SIZE;
     
@@ -234,58 +155,210 @@ int fatRead(rde *rde_ptr, char * buf, int n) {
     }
     
     if (bytes_to_copy < n) {
-        buf[bytes_to_copy] = '\0'; // Null terminate if it's a text file
+        buf[bytes_to_copy] = '\0';
     }
 
-    return bytes_to_copy; // Return number of bytes read
+    return bytes_to_copy;
+}
+
+// Find a free cluster in the FAT
+uint16_t find_free_cluster() {
+    struct boot_sector *bs = (struct boot_sector*)boot_sector;
+    uint16_t *fat = (uint16_t*)fat_table;
+    
+    // Start from cluster 2 (first usable cluster)
+    int max_clusters = (bs->num_sectors_per_fat * 512) / 2;
+    for (int i = 2; i < max_clusters; i++) {
+        if (fat[i] == 0) {
+            return i;
+        }
+    }
+    return 0; // No free cluster found
+}
+
+// Find or create an RDE for a file
+rde* find_or_create_rde(char *filename) {
+    rde *rde_entries = (rde *)root_directory_region;
+    struct boot_sector *bs = (struct boot_sector*)boot_sector;
+    int max_entries = bs->num_root_dir_entries;
+    
+    // Parse filename into name and extension
+    char target_name[8]; 
+    for (int i = 0; i < 8; i++) target_name[i] = ' ';
+    char target_ext[3]; 
+    for (int i = 0; i < 3; i++) target_ext[i] = ' ';
+
+    int dot_pos = -1;
+    for (int i = 0; filename[i] != '\0'; i++) {
+        if (filename[i] == '.') {
+            dot_pos = i;
+            break;
+        }
+    }
+
+    int name_len = (dot_pos >= 0) ? dot_pos : 0;
+    if (dot_pos < 0) {
+        while (filename[name_len] != '\0') name_len++;
+    }
+    if (name_len > 8) name_len = 8;
+
+    for (int i = 0; i < name_len; i++) {
+        target_name[i] = (filename[i] >= 'a' && filename[i] <= 'z') ? (filename[i] - 32) : filename[i];
+    }
+
+    if (dot_pos >= 0) {
+        int ext_start = dot_pos + 1;
+        for (int i = 0; i < 3 && filename[ext_start + i] != '\0'; i++) {
+            target_ext[i] = (filename[ext_start + i] >= 'a' && filename[ext_start + i] <= 'z') 
+                ? (filename[ext_start + i] - 32) 
+                : filename[ext_start + i];
+        }
+    }
+    
+    // First, try to find existing entry
+    for (int k = 0; k < max_entries; k++) {
+        if (rde_entries[k].file_name[0] == 0x00 || 
+            (unsigned char)rde_entries[k].file_name[0] == 0xE5) {
+            continue;
+        }
+        
+        int name_match = 1;
+        for (int i = 0; i < 8; i++) {
+            if (rde_entries[k].file_name[i] != target_name[i]) {
+                name_match = 0;
+                break;
+            }
+        }
+        
+        int ext_match = 1;
+        for (int i = 0; i < 3; i++) {
+            if (rde_entries[k].file_extension[i] != target_ext[i]) {
+                ext_match = 0;
+                break;
+            }
+        }
+        
+        if (name_match && ext_match) {
+            return &rde_entries[k];
+        }
+    }
+    
+    // File doesn't exist, create new entry
+    for (int k = 0; k < max_entries; k++) {
+        if (rde_entries[k].file_name[0] == 0x00 || 
+            (unsigned char)rde_entries[k].file_name[0] == 0xE5) {
+            // Found free slot
+            for (int i = 0; i < 8; i++) rde_entries[k].file_name[i] = target_name[i];
+            for (int i = 0; i < 3; i++) rde_entries[k].file_extension[i] = target_ext[i];
+            rde_entries[k].attribute = 0;
+            rde_entries[k].reserved1 = 0;
+            rde_entries[k].creation_timestamp = 0;
+            rde_entries[k].creation_time = 0;
+            rde_entries[k].creation_date = 0;
+            rde_entries[k].access_date = 0;
+            rde_entries[k].reserved2 = 0;
+            rde_entries[k].modified_time = 0;
+            rde_entries[k].modified_date = 0;
+            rde_entries[k].cluster = 0;
+            rde_entries[k].file_size = 0;
+            
+            return &rde_entries[k];
+        }
+    }
+    
+    return (rde*)0; // No free RDE slots
+}
+
+int fatWrite(char *filename, char *data, int size) {
+    // Find or create RDE for this file
+    rde *entry = find_or_create_rde(filename);
+    if (!entry) {
+        return -1; // No free directory entries
+    }
+    
+    struct boot_sector *bs = (struct boot_sector*)boot_sector;
+    
+    // If file needs a new cluster or doesn't have one
+    if (entry->cluster == 0 || size > CLUSTER_SIZE) {
+        uint16_t cluster = find_free_cluster();
+        if (cluster == 0) {
+            return -2; // No free clusters
+        }
+        
+        // Mark cluster as end-of-chain in FAT
+        uint16_t *fat = (uint16_t*)fat_table;
+        fat[cluster] = 0xFFFF;
+        
+        entry->cluster = cluster;
+    }
+    
+    // Update file size
+    entry->file_size = size;
+    
+    // Write data to cluster
+    int first_sector = data_region_start + (entry->cluster - 2) * bs->num_sectors_per_cluster;
+    
+    char cluster_buf[CLUSTER_SIZE];
+    // Clear buffer
+    for (int i = 0; i < CLUSTER_SIZE; i++) cluster_buf[i] = 0;
+    
+    // Copy data to buffer
+    int bytes_to_write = (size < CLUSTER_SIZE) ? size : CLUSTER_SIZE;
+    for (int i = 0; i < bytes_to_write; i++) {
+        cluster_buf[i] = data[i];
+    }
+    
+    // Write sectors
+    // NOTE: This requires implementing sector_write which needs ata_lba_write
+    // For now, we'll just show the structure
+    for (int i = 0; i < bs->num_sectors_per_cluster; i++) {
+        sector_write(first_sector + i, cluster_buf + (i * SECTOR_SIZE));
+    }
+    
+    // Write updated root directory back to disk
+    sector_write(root_dir_region_start, root_directory_region);
+    
+    // Write updated FAT back to disk
+    int fat_start = 2048 + bs->num_reserved_sectors;
+    for (int i = 0; i < bs->num_sectors_per_fat && i < 32; i++) {
+        sector_write(fat_start + i, fat_table + (i * 512));
+    }
+    
+    return bytes_to_write;
 }
 
 int main() {
-
     ansi_init();
 
     esp_printf(MyPutC, "Booting kernel...\n");
-
-
-    // Three calls to FAT32 functions
-    // fatInit() // Initializes the FAT filesystem driver by reading the superblock (aka boot sector) and FAT into memory.
-    // fatOpen() // Opens a file in a FAT filesystem on disk.
-    // fatRead() // Reads data from a file into a buffer
     
-    fatInit(); // Initializes the FAT filesystem driver by reading the superblock (aka boot sector) and FAT into memory.
+    fatInit();
 
-    rde *file = fatOpen("file.txt"); // Opens a file in a FAT filesystem on disk
+    rde *file = fatOpen("file.txt");
     if (file) {
         char dataBuf[100];
-        int n = fatRead(file, dataBuf, sizeof(dataBuf) - 1); // Reads data from a file into a buffer
+        int n = fatRead(file, dataBuf, sizeof(dataBuf) - 1);
         if (n > 0) {
-            dataBuf[n] = '\0'; // Null terminate the string
-            // esp_printf(MyPutC, "Read %d bytes from file.\n", n);
+            dataBuf[n] = '\0';
         } else {
             esp_printf(MyPutC, "Error reading file.\n");
         }
     } else {
-        esp_printf(MyPutC, "File not found.\n");
+        esp_printf(MyPutC, "File not found (will be created on save).\n");
     }
-
     
-    
-    
-    
-    
-    // DEBUGGING: Echo Testing for keyboard.c
     esp_printf(MyPutC, "Press 1 for KILO editor, 2 for keyboard debug: ");
     int choice = kbd_read_char();
     MyPutC('\n');
 
-    ansi_init(); // Reinitialize ANSI terminal state
+    ansi_init();
 
     if (choice == '1') {
         esp_printf(MyPutC, "Starting KILO editor...\n");
         kilo_run("file.txt");
         esp_printf(MyPutC, "KILO exited.\n");
     } else {
-        esp_printf(MyPutC, "Keyboard debug mode. Press keys (Ctrl+Q to see quit attempt).\n");
+        esp_printf(MyPutC, "Keyboard debug mode. Press keys (Ctrl+Q to exit).\n");
         while (1) {
             int c = kbd_read_char();
             
@@ -293,14 +366,11 @@ int main() {
                 MyPutC('\r');
                 MyPutC('\n');
             } else if (c == '\b' || c == 127) {
-                // Backspace - show it working
                 esp_printf(MyPutC, "[BS]");
                 MyPutC('\b');
             } else if (c >= 32 && c < 127) {
-                // Printable characters
                 MyPutC(c);
             } else if (c >= 1000) {
-                // Arrow keys and special keys
                 switch(c) {
                     case 1000: esp_printf(MyPutC, "[LEFT]"); break;
                     case 1001: esp_printf(MyPutC, "[RIGHT]"); break;
@@ -314,15 +384,13 @@ int main() {
                     default: esp_printf(MyPutC, "[%d]", c); break;
                 }
             } else if (c == 17) {
-                // Ctrl+Q
                 esp_printf(MyPutC, "[CTRL+Q]");
+                break;
             } else {
-                // Control characters and other non-printables
                 esp_printf(MyPutC, "[0x%x]", c);
             }
         }
     }
     
     return 0;
-    
 }
